59e443f47d760ec617b11d0b35b80fcc244b29e0
PikachuLibertario <lendomanga99@gmail.com>
2022-09-26 09:43:34 -0300

    site 4.0

--- javascript/pesquisa.js
+++ javascript/pesquisa.js
@@ -9,10 +9,12 @@ function pesquisar(){
     x = document.getElementById("nao-achado");
     y = 0;
 
+    if(filtro != ""){
     for(let i=0; i < BooksItens.length;i++){
         links = BooksItens[i].getElementsByTagName("a")[0];
 
         if(links.innerHTML.toUpperCase().indexOf(filtro) == 0){
+            books.style.display="block";
             BooksItens[i].style.display="block"; /*mostra o elemento*/
             y++;
         }
@@ -26,4 +28,18 @@ function pesquisar(){
     else{
         x.style.display="block";
     }
+}
+}
+function limpar(){
+    var a = document.getElementById("pesquisa");
+    a.value='';
+
+    var b = document.getElementById("nao-achado");
+    b.style.display="none";
+
+    var c = document.getElementById("books");
+
+    c.style.display="none";
+
+    a.focus();
 }
